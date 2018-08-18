38adcf3e42ca09f79cb1e7c47c764b7a0c448024
benk691 <benk691@gmail.com>
2018-08-18 13:02:30 -0600

    open #6: Estimated coordinates of the map

--- Pi/Map.py
+++ Pi/Map.py
@@ -7,6 +7,8 @@ class Map:
   '''
   # TODO: Draw ASCII Map Here
 
+  # TODO: Measurements in feet. Convert to meters. Degrees, need radians
+
   #--------------------------------------------------------
   #                    CIRCLES
   #--------------------------------------------------------
@@ -17,28 +19,28 @@ class Map:
   # OQ3C : Outer Quadrant 3 Circle
 
   # IQ1C Constants
-  IQ1C_CENTER = [ 0.0, 0.0 ]
-  IQ1C_RADIUS = 0.0
-  IQ1C_START_ANGLE = 0.0
-  IQ1C_STOP_ANGLE = 0.0
+  IQ1C_CENTER = [ 31.0, 31.0 ] 
+  IQ1C_RADIUS = 23.0
+  IQ1C_START_ANGLE = -90.0
+  IQ1C_STOP_ANGLE = 180.0
 
   # OQ1C Constants
-  OQ1C_CENTER = [ 0.0, 0.0 ]
-  OQ1C_RADIUS = 0.0
-  OQ1C_START_ANGLE = 0.0
-  OQ1C_STOP_ANGLE = 0.0
+  OQ1C_CENTER = [ 31.0, 31.0 ]
+  OQ1C_RADIUS = 39.0
+  OQ1C_START_ANGLE = -90.0
+  OQ1C_STOP_ANGLE = 180.0
 
   # IQ3C Constants
-  IQ3C_CENTER = [ 0.0, 0.0 ]
-  IQ3C_RADIUS = 0.0
-  IQ3C_START_ANGLE = 0.0
-  IQ3C_STOP_ANGLE = 0.0
+  IQ3C_CENTER = [ -31.0, -31.0 ]
+  IQ3C_RADIUS = 23.0
+  IQ3C_START_ANGLE = 90.0
+  IQ3C_STOP_ANGLE = 360.0
 
   # OQ3C Constants
-  OQ3C_CENTER = [ 0.0, 0.0 ]
-  OQ3C_RADIUS = 0.0
-  OQ3C_START_ANGLE = 0.0
-  OQ3C_STOP_ANGLE = 0.0
+  OQ3C_CENTER = [ -31.0, -31.0 ]
+  OQ3C_RADIUS = 39.0
+  OQ3C_START_ANGLE = 90.0
+  OQ3C_STOP_ANGLE = 360.0
 
   #--------------------------------------------------------
   #                    LINES
@@ -54,36 +56,36 @@ class Map:
   # OQ4YL : Outer Quadrant 4 Y Parallel Line
 
   # IQ1XL Constants
-  IQ1XL_START_POINT = [ 0.0, 0.0 ]
-  IQ1XL_END_POINT = [ 0.0, 0.0 ]
+  IQ1XL_START_POINT = [ 8.0, 8.0 ]
+  IQ1XL_END_POINT = [ 31.0, 8.0 ]
 
   # IQ1YL Constants
-  IQ1YL_START_POINT = [ 0.0, 0.0 ]
-  IQ1YL_END_POINT = [ 0.0, 0.0 ]
+  IQ1YL_START_POINT = [ 8.0, 8.0 ]
+  IQ1YL_END_POINT = [ 8.0, 31.0 ]
 
   # OQ2XL Constants
-  OQ2XL_START_POINT = [ 0.0, 0.0 ]
-  OQ2XL_END_POINT = [ 0.0, 0.0 ]
+  OQ2XL_START_POINT = [ -8.0, 8.0 ]
+  OQ2XL_END_POINT = [ -31.0, 8.0 ]
 
   # OQ2YL Constants
-  OQ2YL_START_POINT = [ 0.0, 0.0 ]
-  OQ2YL_END_POINT = [ 0.0, 0.0 ]
+  OQ2YL_START_POINT = [ -8.0, 8.0 ]
+  OQ2YL_END_POINT = [ -8.0, 31.0 ]
 
   # IQ3XL Constants
-  IQ3XL_START_POINT = [ 0.0, 0.0 ]
-  IQ3XL_END_POINT = [ 0.0, 0.0 ]
+  IQ3XL_START_POINT = [ -8.0, -8.0 ]
+  IQ3XL_END_POINT = [ -31.0, -8.0 ]
 
   # IQ3YL Constants
-  IQ3YL_START_POINT = [ 0.0, 0.0 ]
-  IQ3YL_END_POINT = [ 0.0, 0.0 ]
+  IQ3YL_START_POINT = [ -8.0, -8.0 ]
+  IQ3YL_END_POINT = [ -8.0, -31.0 ]
 
   # OQ4XL Constants
-  OQ4XL_START_POINT = [ 0.0, 0.0 ]
-  OQ4XL_END_POINT = [ 0.0, 0.0 ]
+  OQ4XL_START_POINT = [ 8.0, -8.0 ]
+  OQ4XL_END_POINT = [ 31.0, -8.0 ]
 
   # OQ4YL Constants
-  OQ4YL_START_POINT = [ 0.0, 0.0 ]
-  OQ4YL_END_POINT = [ 0.0, 0.0 ]
+  OQ4YL_START_POINT = [ 8.0, -8.0 ]
+  OQ4YL_END_POINT = [ 8.0, -31.0 ]
 
   #-------------------------------------------------------------------------------
   def __init__(self):
@@ -95,6 +97,7 @@ class Map:
     self.oq1c = Circle(Map.OQ1C_CENTER, Map.OQ1C_RADIUS, Map.OQ1C_START_ANGLE, Map.OQ1C_STOP_ANGLE)
     self.iq3c = Circle(Map.IQ3C_CENTER, Map.IQ3C_RADIUS, Map.IQ3C_START_ANGLE, Map.IQ3C_STOP_ANGLE)
     self.oq3c = Circle(Map.OQ3C_CENTER, Map.OQ3C_RADIUS, Map.OQ3C_START_ANGLE, Map.OQ3C_STOP_ANGLE)
+    self.circles = [self.iq1c, self.oq1c, self.iq3c, self.oq3c]
     # Course Lines
     self.iq1xl = Line(Map.IQ1XL_START_POINT, Map.IQ1XL_END_POINT)
     self.iq1yl = Line(Map.IQ1YL_START_POINT, Map.IQ1YL_END_POINT)
@@ -104,6 +107,7 @@ class Map:
     self.iq3yl = Line(Map.IQ3YL_START_POINT, Map.IQ3YL_END_POINT)
     self.oq4xl = Line(Map.OQ4XL_START_POINT, Map.OQ4XL_END_POINT)
     self.oq4yl = Line(Map.OQ4YL_START_POINT, Map.OQ4YL_END_POINT)
+    self.lines = [self.iq1xl, self.iq1yl, self.oq2xl, self.oq2yl, self.iq3xl, self.iq3yl, self.oq4xl, self.oq4yl]
 
   #-------------------------------------------------------------------------------
   def _debugDescription(self):
@@ -112,18 +116,11 @@ class Map:
     @return string describing debug information
     '''
     tabs = 1
-    self.iq1c.setTabs(tabs)
-    self.oq1c.setTabs(tabs)
-    self.iq3c.setTabs(tabs)
-    self.oq3c.setTabs(tabs)
-    self.iq1xl.setTabs(tabs)
-    self.iq1yl.setTabs(tabs)
-    self.oq2xl.setTabs(tabs)
-    self.oq2yl.setTabs(tabs)
-    self.iq3xl.setTabs(tabs)
-    self.iq3yl.setTabs(tabs)
-    self.oq4xl.setTabs(tabs)
-    self.oq4yl.setTabs(tabs)
+    for c in self.circles:
+      c.setTabs(tabs)
+
+    for l in self.lines:
+      l.setTabs(tabs)
     desc = "Map:\n"
     desc += "\tIQ1C: {0}\n".format(self.iq1c)
     desc += "\tOQ1C: {0}\n".format(self.oq1c)
