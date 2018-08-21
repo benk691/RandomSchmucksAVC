81ed0456d4545ad058fd4619be91f840140c2fb4
benk691 <benk691@gmail.com>
2018-08-20 21:00:07 -0600

    open #6: Create Particle Filter constants and Particle Filter class. Predict step is done.

--- Pi/Constants.py
+++ Pi/Constants.py
@@ -37,16 +37,16 @@ class Constants:
   PWM_TURN_CHNL = 15
 
   # Left distance sensor echo pin (this is a GPIO pin #)
-  DIST_LEFT_ECHO_PIN = 13
+  DIST_LEFT_ECHO_PIN = 27
 
   # Left distance sensor trigger pin (this is a GPIO pin #)
-  DIST_LEFT_TRIGGER_PIN = 6
+  DIST_LEFT_TRIGGER_PIN = 17
 
   # Left distance sensor echo pin (this is a GPIO pin #)
-  DIST_RIGHT_ECHO_PIN = 19
+  DIST_RIGHT_ECHO_PIN = 22
 
   # Left distance sensor trigger pin (this is a GPIO pin #)
-  DIST_RIGHT_TRIGGER_PIN = 26
+  DIST_RIGHT_TRIGGER_PIN = 18
 
   # The max distance we want the sensors to detect (meters)
   DIST_MAX_DISTANCE = 2.0
@@ -59,3 +59,49 @@ class Constants:
 
   # Y index in a coordinate list
   Y = 1
+
+  # Heading index in a coordinate list
+  HEADING = 2
+
+  # Weight index in a coordinate list
+  WEIGHT = 3
+
+  #--------------------------------
+  #         Particle Filter
+  #--------------------------------
+  # Number of particles to generate for the particle filter
+  PARTICLE_NUMBER = 10
+
+  # Minimum particle number that we do not want to go below
+  MIN_PARTICLE_NUMBER = 2
+
+  # Length between the two axles on the vehicle (meters)
+  VEHICLE_AXLE_LEN = 0.0
+
+  # Two Points that define the start box, on the map, for the vehicle
+  MAP_START_BOX = [ [0.0, 0.0], [0.0, 0.0] ]
+
+  # The starting heading range for the vehicle
+  MAP_HEADING_RANGE = [ 0.0, 0.0 ]
+
+  # Offset that is applied so we can set 0 degrees with out worrying about North/South
+  MAP_HEADING_OFFSET = 0.0
+
+  # Noise that is applied to account for sensor inaccuracies in the heading
+  HEADING_NOISE = 0.0
+
+  # Noise that is applied to account for sensor inaccuracies in the steering angle
+  STEERING_ANGLE_NOISE = 0.0
+
+  # Noise that is applied to account for sensor inaccuracies in the velocity
+  VELOCITY_NOISE = 0.0
+
+  # Update rate for the particle filter (Hz)
+  PARTICLE_FILTER_UPDATE_RATE = 10
+  
+  # Left Most Steering Angle
+  MAX_LEFT_STEERING_ANGLE = 180
+
+  # Right Most Steering Angle
+  MAX_RIGHT_STEERING_ANGLE = 0
+
---
+++ Pi/ParticleFilter.py
@@ -0,0 +1,158 @@
+import random
+import math
+import time
+from Constants import Constants
+
+class ParticleFilter:
+  #-------------------------------------------------------------------------------
+  def __init__(self, particleNumber, startBox, headingRange):
+    '''
+    Initializes the particle filter
+    @param particleNumber - number of particles to generate
+    @param startBox - the inital box to create particles in. List of two X, Y coordinates defining the corners of the box
+    @param headingRange - the range of headings to create particles with. List of two doubles.
+    '''
+    random.seed()
+    self.particleNumber = particleNumber
+    self.prevTime = time.time()
+    self.currentTime = time.time()
+    self.dt = 0.0
+    self.vehicleVelocity = 0.0
+    self.vehicleHeading = 0.0
+    self.vehicleLeftDistance = 0.0
+    self.vehicleRightDistance = 0.0
+    self.vehicleSteeringAngle = 0.0
+    self._maxSteeringAngle = max(Constants.MAX_LEFT_STEERING_ANGLE, Constants.MAX_RIGHT_STEERING_ANGLE)
+    self._minSteeringAngle = min(Constants.MAX_LEFT_STEERING_ANGLE, Constants.MAX_RIGHT_STEERING_ANGLE)
+    # Particle index: [x, y, heading, weight]
+    self.particles = [ [random.uniform(startBox[0][Constants.X], startBox[1][Constants.X]), random.uniform(startBox[0][Constants.Y], startBox[1][Constants.Y]), random.uniform(headingRange[0], headingRange[1]), 1.0] for i in range(self.particleNumber) ]
+
+  #-------------------------------------------------------------------------------
+  def run(self):
+    '''
+    Run the praticle filter
+    '''
+    while True:
+      self._getVehicleMeasurements()
+      self._predict()
+      self._weight()
+
+  #-------------------------------------------------------------------------------
+  def _predict(self):
+    '''
+    Perform the prediction step. This will predict where the particles are going to go 
+    based on the most recent values read in from the vehicle
+    '''
+    self.dt = self.currentTime - self.prevTime
+    for i in range(self.particleNumber):
+      # Generate a steering angle for the particles
+      genSteeringAngle = random.guass(mu=self.vehicleSteeringAngle, sigma=Constants.STEERING_ANGLE_NOISE)
+      if genSteeringAngle > self._maxSteeringAngle:
+        genSteeringAngle = self._maxSteeringAngle
+      if genSteeringAngle < selg._minSteeringAngle:
+        genSteeringAngle = self,_minSteeringAngle
+      turnRadius = Constants.VEHICLE_AXLE_LEN / math.tan(genSteeringAngle)
+      # Generate a velocity for the particles
+      genVelocity = random.guass(mu=self.vehicleVelocity, sigma=Constants.VELOCITY_NOISE) * self.dt
+      rotatedX, rotatedY = self._rotate(turnRadius * math.sin(genVelocity / turnRadius), -turnRadius * (1 - math.cos(genVelocity / turnRadius)), self.particles[i][Constants.HEADING])
+      self.particles[i][Constants.X] += rotatedX
+      self.particles[i][Constants.Y] += rotatedY
+      self.particles[i][Constants.HEADING] += genVelocity / turnRadius
+
+  #-------------------------------------------------------------------------------
+  def _weight(self):
+    '''
+    Calculates the weights for each particle
+    '''
+    for i in range(self.particleNumber):
+      self.particles[i][Constants.WEIGHT] *= 1.0
+
+  #-------------------------------------------------------------------------------
+  def measure(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def calculateHeadingWeights(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def calculateDistanceWeights(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def normalizeWeights(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def checksumParticles(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def distributeNewParticles(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def getEstiamtedLocation(self):
+    pass
+
+  #-------------------------------------------------------------------------------
+  def _rotate(self, x, y, theta):
+    '''
+    Rotates the X, Y coordinate by theta
+    x - the X coordinate
+    y - the Y coordinate
+    theta - the amount to rotate (radians)
+    '''
+    # TODO: Implement rotation
+    return x, y
+
+  #-------------------------------------------------------------------------------
+  def _getVehicleMeasurements(self):
+    '''
+    Read the sensor measurements from the vehicle
+    '''
+    # TODO: Connect this with the sensor conversion/filtered reading thread
+    self.currentTime = time.time()
+    self.vehicleVelocity = 0.0
+    self.vehicleHeading = 0.0
+    self.vehicleLeftDistance = 0.0
+    self.vehicleRightDistance = 0.0
+    self.vehicleSteeringAngle = 0.0
+
+  #-------------------------------------------------------------------------------
+  def _debugDescription(self):
+    '''
+    Generates debugging information about the particle filter
+    @return string describing debug information
+    '''
+    desc = "Particle Filter:\n"
+    desc += "\tparticleNumber = {0}\n".format(self.particleNumber)
+    desc += "\tprevTime = {0}\n".format(self.prevTime)
+    desc += "\tcurrentTime = {0}\n".format(self.currentTime)
+    desc += "\tdt = {0}\n".format(self.dt)
+    desc += "\tvehicleVelocity = {0}\n".format(self.vehicleVelocity)
+    desc += "\tvehicleHeading = {0}\n".format(self.vehicleHeading)
+    desc += "\tvehicleLeftDistance = {0}\n".format(self.vehicleLeftDistance)
+    desc += "\tvehicleRightDistance = {0}\n".format(self.vehicleRightDistance)
+    desc += "\tvehicleSteeringAngle = {0}\n".format(self.vehicleSteeringAngle)
+    desc += "\tparticles:\n"
+    for p in self.particles:
+      desc += "\t\t[X = {0}, Y = {1}, H = {2}, W = {3}]\n".format(p[0], p[1], p[2], p[3])
+    return desc
+
+  #-------------------------------------------------------------------------------
+  def __repr__(self):
+    '''
+    Gets the string representation of the class
+    @return string representation of the class
+    '''
+    return self._debugDescription()
+  
+  #-------------------------------------------------------------------------------
+  def __str__(self):
+    '''
+    Gets the string representation of the class
+    @return string representation of the class
+    '''
+    return self._debugDescription()
+
