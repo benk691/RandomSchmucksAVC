0f64beffa8be8c58283e4cfe3c489c053be7cad0
benk691 <benk691@gmail.com>
2018-08-24 21:17:44 -0600

    open #2: Copy over logic to get vehicle velocity

--- Pi/SensorConversion.py
+++ Pi/SensorConversion.py
@@ -1,3 +1,6 @@
+import Constants
+import math
+import time
 from Filter import Filter
 from threading import Thread
 
@@ -22,8 +25,6 @@ class SensorConversion(Thread):
     self.dataConsumerThread = dataConsumerThread
     self.shutDown = False
     self.steeringPotValue = -0.0
-    self.leftTachValue = -0.0
-    self.rightTachValue = -0.0
     self.velocity = -0.0
     self.steeringAngle = -0.0
     self.leftDistance = -0.0
@@ -35,16 +36,73 @@ class SensorConversion(Thread):
     self.gyroCal = -0.0
     self.accelCal = -0.0
     self.magCal = -0.0
-    # TODO: Do we want a different filter value for all sensors?
-    self.filter = Filter(0.0)
+    self.distTraveled = 0.0
+    self.velocityFilter = Filter(Constants.VELOCITY_FILTER_A)
+    self.steeringFilter = Filter(Constants.STEERING_FILTER_A)
+    self._loopCount = 0
+    self._prevRightTachValue = 0
+    self._prevLeftTachValue = 0
+    self._leftTachValue = -0.0
+    self._rightTachValue = -0.0
+    self._distStartTime = 0
+    self._rightHigh = 0
+    self._leftHigh = 0
+    self._rightStripCount = 0.0
+    self._leftStripCount = 0.0
+    self._elapsedTime = 0
+    self._startTime = 0
+    self._rightVelocity = 0.0
+    self._leftVelocity = 0.0
 
   #-------------------------------------------------------------------------------
   def run(self):
     '''
     Runs the conversion thread
     '''
+    self._distStartTime = time.time()
+    self._startTime = time.time()
     while not self.shutDown:
-      self.getSensorValues()     
+      self.getSensorValues()
+
+      # TODO: Modularize this
+      if (self._loopCount % 1 == 0):
+        self._prevRightTachValue = self.rightTachValue;
+        self._prevLeftTachValue = self.leftTachValue;
+
+      self.distTraveled += (self.velocity / (time.time() - self._distStartTime))
+
+      # TODO: Clean this up
+      self.steeringPotValue = self.steeringFilter.filter(self.steeringPotValue)
+
+      if self._rightHigh == 0 and self._rightTachValue > Constants.TACH_RIGHT_THRESHOLD_HIGH:
+        self._rightStripCount += 0.5
+        self._rightHigh = 1
+
+      # TODO: Should the comparison be with low instead of high?
+      if self._rightHigh == 1 and self._rightTachValue < Constants.TACH_RIGHT_THRESHOLD_HIGH:
+        self._rightStripCount += 0.5
+        self._rightHigh = 0
+
+      if self._leftHigh == 0 and self._leftTachValue > Constants.TACH_LEFT_THRESHOLD_HIGH:
+        self._leftStripCount += 0.5
+        self._leftHigh = 1
+
+      # TODO: Should the comparison be with low instead of high?
+      if self._leftHigh == 1 and self._leftTachValue < Constants.TACH_LEFT_THRESHOLD_HIGH:
+        self._leftStripCount += 0.5
+        self._leftHigh = 0
+
+      self._loopCount += 1
+
+      if self._loopCount >= Constants.MAX_LOOP_COUNT:
+        self._elapsedTime = (time.time() * 1000) - self._startTime
+        self._startTime = (time.time() * 1000)
+        self._rightVelocity = ((self._rightStripCount / 30.0) * 0.36 * math.pi) / (self._elapsedTime / 1000.0)
+        self._leftVelocity = ((self._leftStripCount / 30.0) * 0.36 * math.pi) / (self._elapsedTime / 1000.0)
+
+        self.velocity = (self._leftVelocity + self._rightVelocity) / 2.0
+        self.velocity = self.velocityFilter.filter(self.velocity)
+
 
   #-------------------------------------------------------------------------------
   def getSensorValues(self):
