2e9b3e3e804e4b05df5fd832d2333aef46b3a077
benk691 <benk691@gmail.com>
2018-09-03 16:24:28 -0600

    open #22: Add a generic publisher class

---
+++ Pi/Publisher.py
@@ -0,0 +1,40 @@
+
+class Publisher:
+  '''
+  Publishes data out to its subscribers
+  '''
+  #-------------------------------------------------------------------------------
+  def __init__(self):
+    '''
+    Initializes the publisher
+    '''
+    self.subsrcibers = dict()
+
+  #-------------------------------------------------------------------------------
+  def register(self, subscriber, callback):
+    '''
+    Registers a subsriber to the publisher
+    @param subscriber - the class that is subscribing to this publisher
+    @param callback - function to call upon state update
+    '''
+    self.subscribers[subscriber] = callback
+
+  #-------------------------------------------------------------------------------
+  def unregister(self, subscriber):
+    '''
+    Unregisters a subscriber from the publisher
+    @param subscriber - class to unregister
+    '''
+    del self.subscribers[subscriber]
+
+  #-------------------------------------------------------------------------------
+  def publish(self, *args):
+    '''
+    Publishes arguments to all its subscribers
+    @param args - list of arguments to upate all the subscribers with
+    '''
+    for subscriber, callback in self.subscribers.items():
+      callback(*args)
+
+  
+
